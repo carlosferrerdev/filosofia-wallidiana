c35abc85dd527f79bb7655d7ac532a0bb21bfa04
Carlos Ferrer <carlosvferrer@gmail.com>
2022-12-16 17:38:53 -0300

    Projeto Filosofia Wallidiana

---
+++ quotes.js
@@ -0,0 +1,50 @@
+const localQuotes = [
+  {
+    text: 'Isso aqui não é Disneylândia, é guerra total!',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'É tempo ruim o tempo todo.',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'Tudo que acontece para os fracos enfraquece e para os fortes, fortalece.',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'Quem tem que ter orelha bonita é modelo, porra',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'Sopa de tamanco é pau puro',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'Nunca aceite conselhos construtivos de quem nunca construiu nada.',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'Nem todo vagabundo é ingrato mas todo ingrato é vagabundo.',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'Quando me perguntam com quem vou lutar, sempre respondo: Não te preocupa, tem duas pernas e dois braços… não vou trazer ninguém da lua.',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'Não existe se, é guerra porra.',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'Odiado por muitos, amado por poucos e respeitado por todos.',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'A burrice e a preguiça são doenças altamente contagiosas.',
+    author: 'Wallid Ismail',
+  },
+  {
+    text: 'É terror e pânico.',
+    author: 'Wallid Ismail',
+  }
+]
